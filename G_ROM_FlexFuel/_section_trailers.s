16cc18927898704dab673822992dded4a14c5105
Garrett Gloceri <garrettgloceri@comcast.net>
2025-05-06 20:32:04 -0400

    Rename and big cleanup

---
+++ G_ROM_FlexFuel/_section_trailers.s
@@ -0,0 +1,17 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!! Markers for end of various code segments.
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+.section RomHole_ForCode,"ax"
+.string "G-ROM FLEX FUEL APP v1.2"
+
+.section RomHole_calibrations, "ax"
+.string "G-ROM FLEX FUEL CAL v1.0"
+
+.section RomHeader,"ax"
+.string "G-ROM_FLEX"
+
+.section CalHeader,"ax"
+.string "G-ROM_FLEX"
+
+.end
